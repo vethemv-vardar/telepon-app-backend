292e875b5c954c4df750e6000b8c23ab65fcfc3b
vethemv-vardar <vethemv@gmail.com>
2026-03-03 20:09:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
   res.send('Sürücü Kursu API Çalışıyor 🚀');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8880;
 
 app.listen(PORT, () => {
   console.log(`Server ${PORT} portunda çalışıyor`);
